5409ced1ff0d7db6df823645e121b4d7c4cb2415
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-09-07 20:09:05 +0200

    Add create endpoint

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
+{
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = [.. request.Genres]
+        };
+
+        await movieRepository.CreateAsync(movie);
+
+        var movieResponse = new MovieResponse
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            YearOfRelease = movie.YearOfRelease,
+            Genres = movie.Genres
+        };
+
+        return Created($"/api/movies/{movieResponse.Id}", movieResponse);
+    }
+}
