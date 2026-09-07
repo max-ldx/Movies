df0355c45c4191ea675203f56b5cc3dc4ff073f5
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-09-07 19:56:39 +0200

    Add movie repository

---
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -0,0 +1,43 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    private readonly List<Movie> _movies = [];
+
+    public Task<bool> CreateAsync(Movie movie)
+    {
+        _movies.Add(movie);
+        return Task.FromResult(true);
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id)
+    {
+        return Task.FromResult(_movies.SingleOrDefault(x => x.Id == id));
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        return Task.FromResult(_movies.AsEnumerable());
+    }
+
+    public Task<bool> UpdateAsync(Movie movie)
+    {
+        var movieIndex = _movies.FindIndex(x => x.Id == movie.Id);
+        if (movieIndex == -1)
+        {
+            return Task.FromResult(false);
+        }
+
+        _movies[movieIndex] = movie;
+        return Task.FromResult(true);
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id)
+    {
+        var removedCount = _movies.RemoveAll(x => x.Id == id);
+        var movieRemoved = removedCount > 0;
+        return Task.FromResult(movieRemoved);
+    }
+}
